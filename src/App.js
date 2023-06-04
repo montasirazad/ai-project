1d5f953c41ce96ff4c6da44fcada427cb03c5f5d
unknown <montasirazad@gmail.com>
2023-06-04 16:45:28 +0600

    preparing repository

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          M-bot project
         </a>
       </header>
     </div>
